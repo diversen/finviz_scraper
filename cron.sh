dfe103e985365b200af53a2977c36ffad55fa4d1
Dennis Iversen <dennis.iversen@gmail.com>
2024-04-11 19:30:32 +0200

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -2,6 +2,6 @@
 rm -rf ./cache
 ./venv/bin/python csv_from_sp500.py
 ./venv/bin/python csv_from_nasdaq.py
-./venv/bin/python csv_from_other.py
+# ./venv/bin/python csv_from_other.py
 ./venv/bin/python csv_from_nasdaq100.py
-./venv/bin/python csv_from_all.py
+# ./venv/bin/python csv_from_all.py
