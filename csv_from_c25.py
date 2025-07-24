cca541e699da7648f42157a1446c641befb3d972
Dennis Iversen <dennis.iversen@gmail.com>
2025-07-24 16:19:13 +0200

    fetch correct tickers

--- csv_from_c25.py
+++ csv_from_c25.py
@@ -1,10 +1,10 @@
-from finviz_scraper.get_tickers import tickers_sp500
+from finviz_scraper.get_tickers import tickers_c25
 from finviz_scraper.finviz import get_tickers_df, export_to_csv
 from datetime import datetime
 
 today = datetime.today().strftime("%Y-%m-%d")
 
-tickers = tickers_sp500()
+tickers = tickers_c25()
 df = get_tickers_df(tickers)
 
 export_to_csv(df, "./csv/" + today + "/c25.csv")
